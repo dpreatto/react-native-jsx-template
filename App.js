43d4a658ce49490c42ff77b222aad0f2e00cb702
Dennis Preatto <d.preatto@netevolution.local>
2021-06-16 13:27:42 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import LandingPage from './components/landing_page/LandingPage';
+
+
+export default function App(){
+  return <LandingPage/>
+};
---
+++ components/landing_page/LandingPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+export default function LandingPage(){
+    return <Text style={{color: 'black'}}>This is the landing page</Text>
+
+}
